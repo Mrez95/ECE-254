2f426b9c2addd07aaf0e00f610da7c1d1b5628d5
timothytong <timothy.tong@hotmail.ca>
2015-07-12 18:16:58 -0400

    Multithreading WIP

--- threads_main.c
+++ threads_main.c
@@ -22,3 +22,40 @@ struct buffer_node {
     struct buffer_node * next;
     int val;
 };
+
+// Global vars
+int N, B, P, C, count;
+buffer_node * head; // head of the linked list
+sem_t sem_buffer; // Used to protect critical regions with regards to the buffer (e.g. modifying count etc.)
+
+int main(int argc, char * argv[]){
+    // format should be ./produce <N> <B> <P> <C>
+    if (argc != 5) {
+        exit(1);
+    }
+    
+    // number of integers the producer should produce
+    N = atoi(argv[1]);
+    // number of integers the message queue can hold
+    B = atoi(argv[2]);
+    // number of producers
+    P = atoi(argv[3]);
+    // number of consumers
+    c = atoi(argv[4]);
+    
+    // check for incorrect parameters
+    if (N < 1 || B < 1 || P < 1 || C < 1){
+        exit(1);
+    }
+    
+    /* Initializing semaphores:
+     * sem_init(sem_t *sem, int pshared, unsigned int value);
+     * value = #threads allowed to be in crit. region
+     * pshread: 0 = shared between threads of a process
+     *         !0 = shared between processes
+     * returns 0 on success
+     */
+    sem_init(&sem_buffer, 0, 1);
+    
+    return 0;
+}
