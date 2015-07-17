79d3a1cd44163a9e1deb7132885372a66ac445de
timothytong <timothy.tong@hotmail.ca>
2015-07-17 01:09:24 -0400

    Completed threads version and modified makefile

--- Makefile
+++ Makefile
@@ -6,11 +6,13 @@ CFLAGS=-Wall -o
 LD=gcc
 LDFLAGS=-g
 
-process:
-	gcc -o producer producer.c -lrt -lpthread
-	gcc -o consumer consumer.c -lrt -lpthread -lm
-	gcc -o produce processes_main.c -lrt -lpthread
+#process:
+#	gcc -o producer producer.c -lrt -lpthread
+#	gcc -o consumer consumer.c -lrt -lpthread -lm
+#	gcc -o produce processes_main.c -lrt -lpthread
 
+threads:
+	gcc -o produce threads_main.c -lrt -lm -lpthread
 
 .PHONY: clean
 clean:
--- threads_main.c
+++ threads_main.c
@@ -9,19 +9,19 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <mqueue.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/time.h>
 #include <time.h>
-#include <pthread.h>
 #include <semaphore.h>
+#include <mqueue.h>
 #include <math.h>
 
-void * produce(char * args);
-void * consume(char * args);
+void * produce(void * id);
+void * consume(void * id);
 double get_time();
+void printBuffer();
 
 struct Buffer_node {
     struct Buffer_node * next;
@@ -31,16 +31,23 @@ struct Buffer_node {
 // Global vars
 int N, B, P, C;
 int count = 0; // size of linked list
-int items_processed = 0;
+int items_produced = 0;
+int items_consumed = 0;
 struct Buffer_node * head; // head of the linked list
 struct Buffer_node * tail; // tail of the linked list
-sem_t sem_crit_region,
-        sem_space_avail,
-        sem_item_avail;
+sem_t *sem_crit_region,
+*sem_space_avail,
+*sem_item_avail;
+
+char * sem_crit_reg_name = "sem_crit";
+char * sem_space_avail_name = "sem_space";
+char * sem_item_avail_name = "sem_item";
 
 int main(int argc, char * argv[]){
-        double t_before;
-        double t_after;
+    double t_before;
+    double t_after;
+    mode_t mode = S_IRUSR | S_IWUSR;
+    
     // format should be ./produce <N> <B> <P> <C>
     if (argc != 5) {
         exit(1);
@@ -60,16 +67,12 @@ int main(int argc, char * argv[]){
         exit(1);
     }
     
-    /* Initializing semaphores:
-     * sem_open(sem_t *sem, int pshared, unsigned int value);
-     * value = #threads allowed to be in crit. region
-     * pshread: 0 = shared between threads of a process
-     *         !0 = shared between processes
-     * returns 0 on success
-     */
+    sem_crit_region = sem_open(sem_crit_reg_name, O_RDWR | O_CREAT, mode, 1);
+    sem_space_avail = sem_open(sem_space_avail_name, O_RDWR | O_CREAT, mode, B);
+    sem_item_avail = sem_open(sem_item_avail_name, O_RDWR | O_CREAT, mode, 0);
     
-    if(sem_init(&sem_crit_region, 0, 1) != 0 || sem_init(&sem_space_avail, 0, B) != 0 || sem_init(&sem_item_avail, 0, 0) != 0){
-        perror("sem_init");
+    if(sem_item_avail == SEM_FAILED || sem_space_avail == SEM_FAILED || sem_crit_region == SEM_FAILED){
+        perror("sem_open");
     }
     
     pthread_t producers_threads[P];
@@ -80,18 +83,20 @@ int main(int argc, char * argv[]){
     int p, c;
     // SPAWN PRODUCER THREADS ==> generates messages
     for (p = 0; p < P; p++) {
-        argv[2] = (char *)&p;
-        pthread_create(&producers_threads[p], NULL, &produce, argv);
+        int * producer_id = malloc(sizeof(int));
+        *producer_id = p;
+        //printf("Spawning producer with id %d \n\r", *producer_id);
+        pthread_create(&producers_threads[p], NULL, produce, producer_id);
     }
     
     // SPAWN CONSUMER THREADS ==> consumes messages
-    for (c = 0; c < P; c++) {
-        argv[2] = (char *)&c;
-        pthread_create(&consumers_threads[p], NULL, &consume, argv);
+    for (c = 0; c < C; c++) {
+        int * consumer_id = malloc(sizeof(int));
+        *consumer_id = c;
+        //printf("Spawning consumer with id %d \n\r", *consumer_id);
+        pthread_create(&consumers_threads[c], NULL, consume, consumer_id);
     }
-    
-    
-    
+
     int p_j, c_j;
     // Join all producer threads
     for (p_j = 0; p_j < P; p_j++) {
@@ -103,14 +108,17 @@ int main(int argc, char * argv[]){
         pthread_join(consumers_threads[c_j], NULL);
     }
     
-
+    
     t_after = get_time();
     
     printf("System execution time: %f seconds\n",
            t_after - t_before);
     
-    if(sem_destroy(&sem_crit_region) != 0 || sem_destroy(&sem_space_avail) != 0 || sem_destroy(&sem_item_avail) != 0){
-        perror("sem_destroy");
+    if(sem_close(sem_crit_region) != 0 || sem_close(sem_space_avail) != 0 || sem_close(sem_item_avail) != 0){
+        perror("sem_close");
+    }
+    if (sem_unlink(sem_crit_reg_name) != 0 || sem_unlink(sem_space_avail_name) != 0 || sem_unlink(sem_item_avail_name) != 0) {
+	perror("sem_unlink");
     }
     return 0;
 }
@@ -122,21 +130,20 @@ double get_time() {
     return (tv.tv_sec + tv.tv_usec / 1000000.0);
 }
 
-void * produce(char * args){
-    printf("produce\r\n");
-    int producer_id = args[2];
-    int i; 
-    for (i = 0; i < N / P; i++) {
+void *produce(void *id){
+    int producer_id = *((int *)id);
+    int i;
+    for (i = producer_id; i < N && items_produced < N; i += P) {
         
-        int message = producer_id + P * i;
+        int message = i;
         struct Buffer_node * new_node = malloc(sizeof(struct Buffer_node));
         new_node->next = NULL;
         new_node->val = message;
         
-        sem_wait(&sem_space_avail);
+        sem_wait(sem_space_avail);
         
-        sem_wait(&sem_crit_region);
-        
+        sem_wait(sem_crit_region);
+        //printf("Producer %d appending %d\n\r", producer_id, message);
         count++;
         // Append new message to tail
         if (head == NULL) {
@@ -146,38 +153,56 @@ void * produce(char * args){
             tail->next = new_node;
             tail = new_node;
         }
+        items_produced++;
+        //printf(" .... Items produced: %d\n\r ", items_produced);
+        //printBuffer();
+        sem_post(sem_crit_region);
         
-        sem_post(&sem_crit_region);
-        
-        sem_post(&sem_item_avail);
+        sem_post(sem_item_avail);
     }
+    
+    //printf("Producer %d exited\n\r", producer_id);
 }
 
-void * consume(char * args){
-    printf("consume\r\n");
+void *consume(void *id){
+    int consumer_id = *((int *)id);
     int message;
-    int consumer_id = args[2];
-    while (items_processed < N) {
-        sem_wait(&sem_item_avail);
-        sem_wait(&sem_crit_region);
-        
-        message = head->val; // take from the head
-        if (count == 1) {
-            head = NULL;
-            tail = NULL;
-        } else {
-            struct Buffer_node * tmp = head;
-            head = head->next;
-            tmp = NULL;
-        }
-        count--;
-        
-        // consume.
-        if (sqrt(message) == (int)(sqrt(message))) {
-            printf("%d %d %d\n\r", consumer_id, message, (int)(sqrt(message)));
-        }
-        
-        sem_post(&sem_crit_region);
-        sem_post(&sem_space_avail);
+    while (items_consumed < N) {
+        sem_trywait(sem_item_avail);
+        sem_wait(sem_crit_region);
+	if(head != NULL){
+            message = head->val; // take from the head
+            //printf("Consumer %d consuming %d\n\r", consumer_id, message);
+            if (count == 1) {
+                free(head);
+                head = NULL;
+                tail = NULL;
+            } else {
+                struct Buffer_node * tmp = head;
+                head = head->next;
+                tmp = NULL;
+            }
+            count--;
+            items_consumed++;
+            // consume.
+            if (sqrt(message) == (int)(sqrt(message))) {
+                printf("%d %d %d\n\r", consumer_id, message, (int)(sqrt(message)));
+            }
+            //printBuffer();
+	}
+        sem_post(sem_crit_region);
+        sem_post(sem_space_avail);
     }
+    //printf("Consumer %d exited\n\r", consumer_id);
 }
+
+void printBuffer(){
+    printf("\n **** ");
+    struct Buffer_node *tmp = head;
+    while (tmp != NULL) {
+        printf("[%d] - ", tmp->val);
+        tmp = tmp->next;
+    }
+    printf("[x], COUNT %d\n\r", count);
+}
+
